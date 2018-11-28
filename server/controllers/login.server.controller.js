a55972d522729d51cf85b40220a2bea1fbfe1b48
Alex Mochrie <alex.mochrie@gmail.com>
2018-11-28 11:02:29 -0500

    Create account 1

---
+++ server/controllers/login.server.controller.js
@@ -0,0 +1,27 @@
+
+/* Dependencies */
+var mongoose = require('mongoose'), 
+    Login = require('../models/login.server.model.js');
+
+exports.create = function(req, res) {
+
+    /* Instantiate a Listing */
+    var account = new Login(req.body);
+    
+    
+    /* Then save the listing */
+    account.save(function(err) {
+        if(err) 
+        {
+            console.log(err);
+            res.status(400).send(err);
+        } 
+        else {
+            res.json(listing);
+        }
+    });
+};
+
+
+
+    
