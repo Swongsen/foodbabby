234f44387b8eef5a5930db12ba518537200c54fb
maxnolan <mjlnolan@gmail.com>
2018-11-27 21:38:59 -0500

    https

--- client/js/factories/listingFactory.js
+++ client/js/factories/listingFactory.js
@@ -1,23 +1,23 @@
 angular.module('listings', []).factory('Listings', function($http) {
   var methods = {
     getAll: function() {
-      return $http.get('http://powerful-beach-46688.herokuapp.com/api/listings');
+      return $http.get('https://powerful-beach-46688.herokuapp.com/api/listings');
     },
 	
 	  create: function(listing) {
-      return $http.post('http://powerful-beach-46688.herokuapp.com/api/listings', listing);
+      return $http.post('https://powerful-beach-46688.herokuapp.com/api/listings', listing);
       }, 
 
     delete: function(id) {
-      return $http.delete('http://powerful-beach-46688.herokuapp.com/api/listings/' + id);
+      return $http.delete('https://powerful-beach-46688.herokuapp.com/api/listings/' + id);
     },
 
     getAllForMap: function() {
-      return $http.get('http://powerful-beach-46688.herokuapp.com/api/listings/mapview');
+      return $http.get('https://powerful-beach-46688.herokuapp.com/api/listings/mapview');
     },
 
     getByFoodTypeForMap: function(foodtype) {
-      return $http.get('http://powerful-beach-46688.herokuapp.com/api/listings/mapview/' + foodtype);
+      return $http.get('https://powerful-beach-46688.herokuapp.com/api/listings/mapview/' + foodtype);
     }
   };
 
